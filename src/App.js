bb688d8c7af8dab24ac6a0eb5a89701611743215
Mahesh G K <mahesh.gk@C-LM-0649.local>
2019-12-17 12:48:25 +0530

    smallcss added with inline and external edit

--- src/App.js
+++ src/App.js
@@ -32,11 +32,20 @@ class App extends Component {
   }
 
   render () {
+    const style={
+        backgroundColor: 'white',
+        font:'inherit',
+        border:'1px solid blue',
+        padding:'10px',
+        cursor:'pointer'
+    }
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>working!</p>
-        <button onClick={() => this.switchNameHandler('Mahesh!!')}>Switch Name</button>
+        <h1>Hi React </h1>
+        <p>working dude!</p>
+        <button 
+        style={style}
+        onClick={() => this.switchNameHandler('Mahesh!!')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
